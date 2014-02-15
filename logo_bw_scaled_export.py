f7e52103ab79d2db59ae50d06536d7b7c52355e6
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2014-02-15 07:03:51 +0100

    wip

--- logo_bw_scaled_export.py
+++ logo_bw_scaled_export.py
@@ -1,13 +1,33 @@
-#!/usr/bin/env python
-
-# Copyright 2012 by Akkana Peck, http://www.shallowsky.com/software/
-# You may use and distribute this plug-in under the terms of the GPL v2
-# or, at your option, any later GPL version.
+    #!/usr/bin/env python
 
 from gimpfu import *
 import os, sys
 
 export_width = 1600
+
+def python_export_bw_scaled_jpeg(img, base, origdir, subdir):
+    subdir_full = os.path.join(origdir,subdir)
+    if not os.path.isdir(subdir_full):
+        os.mkdir(subdir_full)
+    jpgname = os.path.join(subdir_full, base+".jpg")
+    print >>sys.stderr, jpgname
+    img.flatten()
+    pdb.file_jpeg_save( # RUN-NONINTERACTIVE
+        img,
+        pdb.gimp_image_get_active_layer(img),
+        jpgname,
+        jpgname,
+        1,		# kvalitet
+        0,		# utjamning
+        1,		# optimaliser
+        0,		# progressiv
+        "Created with GIMP by ~T~",
+        3,		# subsmp, 3 is best quality (?)
+        1,		# force baseline
+        0,		# restart markers
+        0,		# dct slow
+    )
+    pdb.gimp_image_delete(tmp)
 
 def python_export_bw_scaled(img, drawable) :
     global export_width
@@ -16,54 +36,24 @@ def python_export_bw_scaled(img, drawable) :
     origdir = os.path.dirname(img.filename)
     base, ext = os.path.splitext(os.path.basename(img.filename))
 
+    if not any([l.name == 'vm' for l in img.layers]):
+        # Will this show to the user? TODO
+        raise gimp.error("No layer named 'vm'!")
+
     tmp = img.duplicate()
-    tmp.flatten()
+    for l in tmp.layers:
+        if l.name == 'vm':
+            l.visible = False
+    python_export_bw_scaled_jpeg(tmp, base, origdir, "farge")
 
-    subdir = "farge"
-    subdir_full = os.path.join(origdir,subdir)
-    if not os.path.isdir(subdir_full):
-        os.mkdir(subdir_full)
-    jpgname = os.path.join(subdir_full, base+".jpg")
-    print >>sys.stderr, jpgname
-    pdb.file_jpeg_save( # RUN-NONINTERACTIVE
-        tmp,
-        pdb.gimp_image_get_active_layer(tmp),
-        jpgname,
-        jpgname,
-        1,		# kvalitet
-        0,		# utjamning
-        1,		# optimaliser
-        0,		# progressiv
-        "Created with GIMP by ~T~",
-        3,		# subsmp, 3 is best quality (?)
-        1,		# force baseline
-        0,		# restart markers
-        0,		# dct slow
-    )
-
+    tmp = img.duplicate()
     pdb.gimp_image_scale(tmp, export_width, export_height)
-    # TODO: make logo layer visible
-    subdir = "farge_til_nettbruk"
-    subdir_full = os.path.join(origdir,subdir)
-    if not os.path.isdir(subdir_full):
-        os.mkdir(subdir_full)
-    jpgname = os.path.join(subdir_full, base+".jpg")
-    print >>sys.stderr, jpgname
-    pdb.file_jpeg_save( # RUN-NONINTERACTIVE
-        tmp,
-        pdb.gimp_image_get_active_layer(tmp),
-        jpgname,
-        jpgname,
-        1,		# kvalitet
-        0,		# utjamning
-        1,		# optimaliser
-        0,		# progressiv
-        "Created with GIMP by ~T~",
-        3,		# subsmp, 3 is best quality (?)
-        1,		# force baseline
-        0,		# restart markers
-        0,		# dct slow
-    )
+    for l in tmp.layers:
+        if l.name == 'vm':
+            l.visible = False
+
+
+    python_export_bw_scaled_jpeg(tmp, base, origdir, "farge_til_nettbruk")
 
     # TODO the other four
     # tmp = img.duplicate()
